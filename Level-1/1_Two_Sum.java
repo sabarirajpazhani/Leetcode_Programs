655bd5e3832d04b928e65eb6456f5e092ba1b59a
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-08-25 20:53:20 +0530

    LINK Delete

--- Level-1/1_Two_Sum.java
+++ Level-1/1_Two_Sum.java
@@ -16,5 +16,3 @@ class Solution {
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-//LINK: https://leetcode.com/problems/two-sum/description/
